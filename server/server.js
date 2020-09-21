b3f26ce9d27c96808a1a5dd8b65b8cea76378dc1
Fernando Vazquez <fdovazqc@gmail.com>
2020-09-21 14:46:14 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,42 @@
+require("./config/config");
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/usuario", function (req, res) {
+  res.json("get Usuario");
+});
+
+app.post("/usuario", function (req, res) {
+  let { body } = req;
+
+  if (body.nombre === undefined) {
+    res.status(400).json({
+      ok: false,
+      msj: "El nombre es obligatorio",
+    });
+  } else {
+    res.json(body);
+  }
+});
+
+app.put("/usuario/:id", function (req, res) {
+  let id = req.params.id;
+
+  res.json("put Usuario " + id);
+});
+
+app.delete("/usuario", function (req, res) {
+  res.json("delete Usuario");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor ejecutandose en el puerto ${process.env.PORT}`);
+});
